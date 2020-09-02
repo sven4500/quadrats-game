c4e58ca403ab57d40e9e8cfe9e3e44fc17df4011
sven4500 <sven4500@mail.ru>
2020-09-02 21:49:21 +0300

    Окно имеет размер 3/4 от размера экрана.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,23 @@
 #include <QApplication>
 #include <QDesktopWidget>
 #include <QRect>
-#include "quadrats.h"
+#include <quadrats.h>
 
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
     CQuadrats widget;
 
-    // центрируем окно по середине экрана
-    {
-        QDesktopWidget* const desktop = app.desktop();
+    QDesktopWidget* const desktop = app.desktop();
 
-        if(desktop != 0)
-        {
-            QRect const rect = desktop->availableGeometry();
-            unsigned int const size = std::min(rect.width(), rect.height()) * 0.6;
-            widget.resize(size, size);
-            widget.move(rect.center().x() - size / 2, rect.center().y() - size / 2);
-        }
+    // Центрируем окно по середине экрана.
+    if(desktop != 0)
+    {
+        QRect const rect = desktop->availableGeometry();
+        int const size = std::min(rect.width(), rect.height()) * 0.75;
+
+        widget.resize(size, size);
+        widget.move(rect.center().x() - size / 2, rect.center().y() - size / 2);
     }
 
     widget.show();
